52bc01cbfafeb6b619a936561fc90c1a93749b32
atulgautamg <125824896+atulgautamg@users.noreply.github.com>
2024-08-28 00:28:41 +0530

    Update User.js

--- src/User.js
+++ src/User.js
@@ -85,7 +85,7 @@ const User = () => {
     <div >
     <nav class="navbar navbar-expand-lg nav3">
   <div class="container-fluid ">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">CollegeDunia</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
